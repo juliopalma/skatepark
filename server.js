fa401d5cf6e052f72751167b0df4ab808ddac49c
Julio Palma Quezada <j.palma.quezada@gmail.com>
2022-03-26 20:20:07 -0300

    se agrega imagen en la pantalla de lista de participantes en el index.html

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(flash());
+app.use(express.static('static'));
+
 
 nunjucks.configure('templates', {
     express: app,
