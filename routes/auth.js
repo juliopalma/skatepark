1e788ce6aa6a40b53d5ae5d402df7c072c96c7d7
Julio Palma Quezada <j.palma.quezada@gmail.com>
2022-03-29 18:19:40 -0300

    se agregan validaciones con flash y se arregla maqueta de pantallas

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const { get_user, create_user, eliminar } = require('../db.js');
 const { route } = require('./routes.js');
+const session = require('express-session');
+const flash = require('connect-flash');
 const router = express.Router();
-
+router.use(flash());
 
 router.get('/login', (req, res) => {
     const errors = req.flash('errors');
@@ -11,25 +13,21 @@ router.get('/login', (req, res) => {
 });
 
 router.post('/login', async(req, res) => {
-    // 1. Recuperar los valores del formulario
     const email = req.body.email;
     const password = req.body.password;
 
-    // 2. Validar que usuario sí existe
     const user = await get_user(email);
     if (!user) {
         req.flash('errors', 'Usuario no existe o contraseña incorrecta');
         return res.redirect('/login');
     }
 
-    // 3. Validar que contraseña coincida con lo de la base de datos
     const son_iguales = await bcrypt.compare(password, user.password);
     if (!son_iguales) {
         req.flash('errors', 'Usuario no existe o contraseña incorrecta');
         return res.redirect('/login');
     }
 
-    // 4. Guardamos el usuario en sesión
     req.session.user = user
     res.redirect('/');
 });
@@ -40,7 +38,7 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async(req, res) => {
-    // 1. Recuperamos los valores del formulario
+
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
@@ -52,19 +50,16 @@ router.post('/register', async(req, res) => {
     const estado = req.body.estado;
 
     const ext = foto.split('.').slice(-1)[0].toLowerCase();
-    //validar que la extencion es permitida, jpg, png, jpeg, bmp
     if (ext != 'jpg' && ext != 'png' && ext != 'jpeg' && ext != 'bmp') {
         req.flash('errors', 'La extension del archivo no es la correcta');
         return res.redirect('/register');
     }
 
-    // 2. validar que contraseñas sean iguales
     if (password != password_confirm) {
         req.flash('errors', 'La contraseñas no coinciden');
         return res.redirect('/register');
     }
 
-    // 3. validar que email no exista previamente
     const user = await get_user(email);
     if (user) {
         req.flash('errors', 'Usuario ya existe o contraseña incorrecta');
@@ -76,8 +71,6 @@ router.post('/register', async(req, res) => {
 
     await fileimagen.mv(`static/imgs/${foto}`);
 
-
-    // 4. Guardo el nuevo usuario en sesión
     req.session.user = { name, email, anio_exper, especialidad }
     res.redirect('/login')
 });
@@ -89,38 +82,6 @@ router.get('/eliminar', async(req, res) => {
 
     res.redirect('/login');
 });
-
-
-
-
-
-
-
-/* router.post('/imagen', async(req, res) => {
-
-    const img = req.files.target_file;
-    const posicion = req.body.posicion;
-
-    if (parseInt(posicion) <= 8) {
-        await img.mv(`static/imgs/imagen-${posicion}.jpg`);
-    } else {
-        console.log("Debe ingresar una posicion entre 1 y 8");
-    }
-    //res.send('imagen subida de forma exitosa al collage');
-    res.redirect('/collage');
-
-}); */
-
-/* router.post('/guardarimg', async(req, res) => {
-
-    const img = req.files.target_file;
-
-    await img.mv(`static/imgs/`);
-
-    res.redirect('/');
-
-});
- */
 
 
 module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const flash = require('connect-flash');
+const router = express.Router();
+router.use(flash());
 
 const { get_users, update, get_user, set_auth } = require('../db.js');
-const router = express.Router();
-
 
 function protected_routes(req, res, next) {
     if (!req.session.user) {
         req.flash('errors', 'Debe ingresar al sistema primero');
 
-        //BORRAR LA SIGUIENTE LINEA AL TERMINAR DE CARGAR LA PAGINA DE DATOS 
         return res.redirect('/login')
     }
     next();
@@ -28,7 +29,7 @@ router.get('/datos', protected_routes, async(req, res) => {
 });
 
 router.post('/datos', protected_routes, async(req, res) => {
-    // 1. Recuperamos los valores del formulario
+
     const email = req.session.user.email;
     const name = req.body.name;
     const password = req.body.password;
@@ -36,20 +37,17 @@ router.post('/datos', protected_routes, async(req, res) => {
     const anos_experiencia = req.body.anos_experiencia;
     const especialidad = req.body.especialidad;
 
-    // 1. Validamos que ambas contraeeña coincidan
     const user = await get_user(email);
     if (!user) {
         req.flash('errors', 'Usuario no existe o contraseña incorrecta');
         return res.redirect('/login');
     }
 
-    // 3. Validar que contraseña coincida con lo de la base de datos
     if (password != password_confirm) {
         req.flash('errors', 'Usuario no existe o contraseña incorrecta');
         return res.redirect('/login');
     }
 
-    // 2. Actualizar al usuario actual
     const password_encrypt = await bcrypt.hash(password, 10)
     await update(email, name, password_encrypt, parseInt(anos_experiencia), especialidad);
     req.session.user = { email, nombre: name, anos_experiencia, especialidad }
